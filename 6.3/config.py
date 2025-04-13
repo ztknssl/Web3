3a29b6faed02804de15f8becd9063072af287e9e
ztknssl <davydov.andrey37@gmail.com>
2025-04-13 22:42:43 +0300

    3 task with logger

---
+++ 6.3/config.py
@@ -0,0 +1,10 @@
+LOGS = 'logs.log'
+
+rpc_uri = 'https://1rpc.io/eth'
+
+wallets_list = [
+    '0x742d35Cc6634C0532925a3b844Bc454E4438f44e',
+    '0xfe9e8709d3215310075d67e3ed32a380ccf451c8',
+    '0xdc76cd25977e0a5ae17155770273ad58648900d3'
+]
+
---
+++ 6.3/main.py
@@ -0,0 +1,41 @@
+from web3 import AsyncWeb3, AsyncHTTPProvider
+from config import rpc_uri, wallets_list
+import asyncio
+from logger import logger
+import sys
+
+
+if sys.platform:
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+w3 = AsyncWeb3(AsyncHTTPProvider(endpoint_uri=rpc_uri))
+
+
+""" Функция display_balance получает на вход адрес кошелька, преобразует его в checksum и выводит баланс в консоль """
+async def display_balance(wallet: str) -> None:
+    balance = await w3.eth.get_balance(w3.to_checksum_address(wallet))
+    eth_balance = w3.from_wei(balance, 'ether')
+    logger.info(f'Адрес: {wallet}, Баланс: {eth_balance} ETH')
+
+
+async def main():
+    try:
+        await asyncio.gather(*(display_balance(wallet) for wallet in wallets_list))
+    except ConnectionError:
+        logger.error('Ошибка подключения к сети Ethereum Mainnet')
+    except ValueError as e:
+        logger.error(f'Ошибка в формате данных: {str(e)}')
+    except Exception as e:
+        logger.error(f'Непредвиденная ошибка: {str(e)}')
+    finally:
+        await w3.provider.disconnect()
+
+
+asyncio.run(main())
+
+
+
+
+
+
